5f02ad75decd6879449f0a73e94cbb36610d52fb
hari <kamsuhari123@gmail.com>
2026-06-27 11:45:13 +0530

    fixed report name

--- cypress.config.js
+++ cypress.config.js
@@ -4,42 +4,42 @@ const Mochawesome = require('mochawesome')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.automationexercise.com',
-    defaultCommandTimeout:6000,
-    pageLoadTimeout:10000,
-    video:true,
-    screenshotOnRunFailure:true,
-    viewportWidth:1280,
-    viewportHeight:720,
+    defaultCommandTimeout: 6000,
+    pageLoadTimeout: 10000,
+    video: true,
+    screenshotOnRunFailure: true,
+    viewportWidth: 1280,
+    viewportHeight: 720,
 
 
 
-    reporter: 'Mochawesome',
-    reporterOptions:{
-      reportDir:"cypress/reports",
-      html:true,
-      json:false,
-      overwrite:true,
-      reportFilename:"[name]_[datetime]",
-      timestamp:"YYYY-mm-dd_HH-MM-ss"
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      html: true,
+      json: false,
+      overwrite: true,
+      reportFilename: "[name]_[datetime]",
+      timestamp: "YYYY-mm-dd_HH-MM-ss"
     },
 
     env:
     {
-      activeEnv : 'dev',
+      activeEnv: 'dev',
 
-      int:{
+      int: {
 
-        email:"mom@gmail.com",
-        password:"mom@123"
+        email: "mom@gmail.com",
+        password: "mom@123"
       },
 
       dev:
       {
-        email:"mom@gmail.com",
-        password:"mom@123"
+        email: "mom@gmail.com",
+        password: "mom@123"
       }
-      
+
     },
-    setupNodeEvents(on, config) {}
+    setupNodeEvents(on, config) { }
   }
 })
